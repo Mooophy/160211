5a69ec0002226e92ba5c8ed6884a2029a9763675
Yue Wang <yue.wang.programmer@gmail.com>
2018-07-29 16:49:17 +1200

    Update A1.m

--- A1.m
+++ A1.m
@@ -1,5 +1,37 @@
-clear;
+% Part II
 clock;
+format rational;
+
+% Problem 1
+clear;
+
+A = [3 1 -1; -6 3 2; -3 2 1];
+U = A;
+M = zeros(3);       % M holds multiplies value
+V = zeros(3);       % V holds pivot entry positions
+
+M(1, 1) = - U(1, 1) / U(2, 1);
+M(3, 1) = - U(3, 1) / U(2, 1);
+U(1, :) = M(1, 1) * U(2, :) + U(1, :);
+U(3, :) = M(3, 1) * U(2, :) + U(3, :);
+V(2, 1) = 1;
+
+M(3, 2) = - U(3, 2) / U(1, 2);
+U(3, :) = M(3, 2) * U(1, :) + U(3, :);
+V(1, 2) = 1;
+
+V(3, 3) = 1;
+
+I = eye(3);
+P = I([2 1 3], :);  %P
+
+U = P * U;          %U 
+
+L = P * (V - M);    %L
+
+% Problem 2
+
+clear;
 
 disp('2.a ======')
 
@@ -14,6 +46,6 @@ disp(B1);
 
 B2 = eye(4); 
 for i = 1 : n 
-    B2(i, :) = A .^ (i - 1); 
+    B2(i,:) = A .^ (i - 1); 
 end
 disp(B2);
