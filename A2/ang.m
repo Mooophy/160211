bdd4404902bd9117ae1e1872a517ac08b853a07c
Yue Wang <Moophyalan@gmail.com>
2018-08-29 11:22:11 +1200

    Create ang.m

---
+++ A2/ang.m
@@ -0,0 +1,10 @@
+function y = ang(a,b)
+
+%Retuns angle between the two nonzero vectors
+
+if(all(a == 0) ||all(b == 0))
+    disp 'Error! Angle between zero vector is not defined.'
+    return
+end
+
+y=acosd(dot(a,b)/norm(a)*norm(b));
