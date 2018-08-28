855fe169e214c540faea1b0be287b5bc921140fd
Yue Wang <Moophyalan@gmail.com>
2018-08-29 11:30:18 +1200

    Update ang.m

--- A2/ang.m
+++ A2/ang.m
@@ -2,7 +2,7 @@ function y = ang(a,b)
 
 %Retuns angle between the two nonzero vectors
 
-if(all(a == 0) ||all(b == 0))
+if(all(a == 0) || all(b == 0))
     disp 'Error! Angle between zero vector is not defined.'
     return
 end
