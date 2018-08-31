dc0398fa257827fe4df4bafc0538fb467857107e
Yue Wang <Moophyalan@gmail.com>
2018-08-31 13:55:40 +1200

    Update short.m

--- A3/short.m
+++ A3/short.m
@@ -1,1 +1,13 @@
-%
+function y = short(A,x)
+
+%Retuns
+
+Q = orth(A);
+
+projection = @(v)((dot(x,v)/dot(v,v))*v);
+
+s = sum([ projection(Q(:, 1)),  projection(Q(:, 2))]);
+
+y = norm(x - s);
+
+end
