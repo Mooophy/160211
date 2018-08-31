55520e0e551a713b1dce29c95fcae7500dc34afe
Yue Wang <Moophyalan@gmail.com>
2018-08-31 16:08:14 +1200

    Update htr.m

--- A3/htr.m
+++ A3/htr.m
@@ -1,18 +1,18 @@
 function y = htr(u)
 
-%Returns Householder transformation that transforms u to [t, 0, 0 ...]
+%Returns Householder transformation that transform u to [t, 0, 0 ...]
 
 [r, ~] = size(u);
-
-if(all(u(2:end) == 0))
-    y = eye(r);
-    return
-end
 
 v = u;
 v(:) = 0;
 v(1) = norm(u);
 w = u - v;
+
+if(all(w==0))
+    y = eye(r);
+    return
+end
 
 y = eye(r) - 2*(w * w')/(norm(w)^2);
 
