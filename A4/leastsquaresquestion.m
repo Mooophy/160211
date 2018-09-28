d546941be4a44beae20140e8aa048c9cfcd5a352
Yue Wang <Moophyalan@gmail.com>
2018-09-28 16:01:08 +1200

    Update leastsquaresquestion.m

--- A4/leastsquaresquestion.m
+++ A4/leastsquaresquestion.m
@@ -1,6 +1,13 @@
+function v = leastsquaresquestion(x, y, d)
 
-function y = leastsquaresquestion(u)
+%
 
-%Returns Householder transformation that transform u to [t, 0, 0 ...]
+A = zeros(length(x), d+1);
+
+for i = 1 : length(x)
+    A(i,:)= x(i) .^ (0:d);
+end
+
+v = linsolve(A' * A, A' * y);
 
 end
