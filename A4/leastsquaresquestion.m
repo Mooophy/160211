3cdc724842c5837d3dcdd90d93ba647447b83fbb
Yue Wang <Moophyalan@gmail.com>
2018-10-01 14:48:31 +1300

    Update leastsquaresquestion.m

--- A4/leastsquaresquestion.m
+++ A4/leastsquaresquestion.m
@@ -1,13 +1,38 @@
-ts = 0.2 : 0.3 : 2;
-ys = [3.6766 2.1631 1.1797 0.7326 0.3455 0.3332 0.0996];
+t = 0.2 : 0.3 : 2;
+y = [3.6766 2.1631 1.1797 0.7326 0.3455 0.3332 0.0996];
 
-v1 = fit(ts', ys', 1);
+v1 = fit(t', y', 1);
 
-disp(v1);
+C = exp(v1(1));
+k = v1(2);
+
+disp([C k]);
+
+f1 = @(x) C * exp(k * x);
+y1 = zeros(1, length(ys));
+for i = 1:length(y1)
+    y1(i) = f1(ts(i));
+end
+disp(y1);
+
+r1 = residual(y, y1);
+disp(r1)
+
+disp('=================')
+
+v2 = fit(t', y', 2);
+disp(v2)
+f2 = @(x) v2(1) + v2(2)*x + v2(3)*(x^2);
+y2 = zeros(1, length(ys));
+for i = 1:length(y2)
+    y2(i) = f2(ts(i));
+end
+disp(y2);
+
+r2 = residual(y, y2);
+disp(r2)
 
 function v = fit(x, y, d)
-
-%
 
 A = zeros(length(x), d+1);
 
@@ -17,4 +42,8 @@ end
 
 v = linsolve(A' * A, A' * y);
 
+end
+
+function r = residual(y1, y2)
+r = sum((y1 - y2).^ 2);
 end
