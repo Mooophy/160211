cce6563e4789bc8ed53ba6de3874aad2422a0517
Yue Wang <Moophyalan@gmail.com>
2018-10-02 12:47:00 +1300

    Update moore.m

--- A4/moore.m
+++ A4/moore.m
@@ -1,5 +1,10 @@
-function y = moore(A)
+function G = moore(A)
 
-%
+% Returns Moore-Penrose inverse of a matrix A
+
+R = rref(A);
+C = R(1:rank(A), :);
+F = C * A';
+G = C' * inv(F * F') * F;
 
 end
